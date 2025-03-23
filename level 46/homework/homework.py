ed4389e425e9435053fa6206d61f7eaf9bb87d70
nikoarakhamia <arakhamianiko220@gmail.com>
2025-03-23 22:38:55 +0400

    second commit

---
+++ level 46/homework/homework.py
@@ -0,0 +1,32 @@
+# 1 codewars
+
+def solution(text, ending):
+    return text.endswith(ending)
+
+# 2 codewars
+
+def basic_op(operator, value1, value2):
+    if operator == "+":
+        return value1 + value2
+    elif operator == "-":
+        return value1 - value2
+    elif operator == "":
+        return value1 value2
+    else:
+        return value1 / value2
+    
+# 3 codewars
+
+def digitize(n):
+    return [int(digit) for digit in str(n)[::-1]]
+
+# 4 codewars 
+
+def century(year):
+    return (year + 99) // 100
+
+# 5 codewars
+
+def litres(time):
+    return time // 2
+
